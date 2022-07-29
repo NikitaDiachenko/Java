57be33fce3e98030423ae037d9ba03dfaea8d4d5
Nikita Diachenko <nicklockpick@gmail.com>
2022-07-29 15:29:33 +0300

    TwoStrings program commited

---
+++ TwoStrings/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String a = "myFirstString";
+        String b = "my Second String";
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
