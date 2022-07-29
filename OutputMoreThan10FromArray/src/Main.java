e3c08e1033ad80e25f33fa1a7a30fb8c9050e364
Nikita Diachenko <nicklockpick@gmail.com>
2022-07-29 19:00:04 +0300

    OutputMoreThan10FromArray

---
+++ OutputMoreThan10FromArray/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int [] arr = {1,5,33,12,88,9,192,123,567,88,44,32};
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] > 10){
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
